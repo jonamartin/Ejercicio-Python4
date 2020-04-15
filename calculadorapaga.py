848e896949ae17b241aa7052f354c6fab21cbc4c
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-04-14 23:39:25 -0300

    Add files via upload
    Solución propuesta ejercicio:
    Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.

---
+++ calculadorapaga.py
@@ -0,0 +1,6 @@
+horas = float(input("Ingrese el numero de horas trabajadas: "))
+costo = float(input("Ingrese el costo por hora trabajada: "))
+
+paga = str(horas*costo)
+
+print("El total a cobrar es de: $"+paga)
